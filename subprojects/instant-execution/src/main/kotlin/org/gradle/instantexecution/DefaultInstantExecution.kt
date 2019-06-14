04b5227f756e14bba0cefdac41a9548ac3e9bf67
Paul Merlin <paul@gradle.com>
2019-06-14 15:44:40 +0200

    Let instant execution use classpath from loaders used in this build
    as a poor's man solution to missing classpath elements for closures,
    lambdas and types from scripts.
    
    This commit doesn't let instant execution reuse existing classloaders
    but simply use a more complete classpath as a first step.
    
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/instant-execution/src/main/kotlin/org/gradle/instantexecution/DefaultInstantExecution.kt
+++ subprojects/instant-execution/src/main/kotlin/org/gradle/instantexecution/DefaultInstantExecution.kt
@@ -17,6 +17,9 @@
 package org.gradle.instantexecution
 
 import org.gradle.api.Task
+import org.gradle.api.internal.GradleInternal
+import org.gradle.api.internal.initialization.loadercache.ClassLoaderCache
+import org.gradle.api.internal.initialization.loadercache.ClassLoaderCacheInternal
 import org.gradle.api.invocation.Gradle
 import org.gradle.api.logging.Logging
 import org.gradle.initialization.InstantExecution
@@ -116,8 +119,8 @@ class DefaultInstantExecution(
                     val scheduledTasks = build.scheduledTasks
                     writeRelevantProjectsFor(scheduledTasks)
 
-                    val tasksClassPath = classPathFor(scheduledTasks)
-                    writeClassPath(tasksClassPath)
+                    val classPath = classPathFor(build.rootProject.gradle)
+                    writeClassPath(classPath)
 
                     TaskGraphCodec().run {
                         writeTaskGraphOf(build, scheduledTasks)
@@ -145,8 +148,8 @@ class DefaultInstantExecution(
                     build.autoApplyPlugins()
                     build.registerProjects()
 
-                    val tasksClassPath = readClassPath()
-                    val taskClassLoader = classLoaderFor(tasksClassPath)
+                    val classPath = readClassPath()
+                    val taskClassLoader = classLoaderFor(classPath)
                     initialize(build::getProject, taskClassLoader)
 
                     val scheduledTasks = TaskGraphCodec().run {
@@ -230,11 +233,11 @@ class DefaultInstantExecution(
         host.classLoaderFor(classPath)
 
     private
-    fun classPathFor(tasks: List<Task>) = DefaultClassPath.of(
+    fun classPathFor(gradle: GradleInternal) = DefaultClassPath.of(
         linkedSetOf<File>().also { classPathFiles ->
-            for (task in tasks) {
+            gradle.cachedClassLoadersUsedInThisBuild().forEach { loader ->
                 ClasspathUtil.collectClasspathOf(
-                    task.javaClass.classLoader,
+                    loader,
                     classPathFiles
                 )
             }
@@ -311,3 +314,8 @@ inline fun <T> T.withGradle(
         }
     }
 }
+
+
+private
+fun GradleInternal.cachedClassLoadersUsedInThisBuild(): List<ClassLoader> =
+    (services.get(ClassLoaderCache::class.java) as ClassLoaderCacheInternal).usedInThisBuild()
